1ed8dd6c73cd942a109562112afb75684268a553
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-11-02 13:41:28 +0000

    Attemped to Seed the database but got errors

---
+++ ClubsContext.cs
@@ -0,0 +1,28 @@
+﻿using ClubDomain.Classes.ClubModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDomain.Classes.ClubModels
+{
+    public class ClubsContext : DbContext
+    {
+
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<ClubEvent> ClubEvents { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<EventAttendnace> EventAttendances { get; set; }
+        public ClubsContext()
+            : base("DefaultConnection")
+        {
+        }
+        public static ClubsContext Create()
+        {
+            return new ClubsContext();
+        }
+
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,73 @@
+﻿namespace Week3ClubDomain2122.Classes.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ClubDomain.Classes.ClubModels.ClubsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ClubDomain.Classes.ClubModels.ClubsContext context)
+        {
+            var manager =
+                new UserManager<ApplicationUser>(
+                    new UserStore<ApplicationUser>(context));
+
+            var roleManager =
+                new RoleManager<IdentityRole>(
+                    new RoleStore<IdentityRole>(context));
+
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "Admin" }
+                );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "ClubAdmin" }
+                );
+            context.Roles.AddOrUpdate(r => r.Name,
+                new IdentityRole { Name = "member" }
+                );
+
+            PasswordHasher ps = new PasswordHasher();
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "powell.paul@itsligo.ie",
+                    Email = "powell.paul@itsligo.ie",
+                    EmailConfirmed = true,
+                    JoinDate = DateTime.Now,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    FirstName = "Paul",
+                    Surname = "Powell",
+                    PasswordHash = ps.HashPassword("Rad3022021$1")
+                });
+
+            context.Users.AddOrUpdate(u => u.UserName,
+                new ApplicationUser
+                {
+                    UserName = "radp2016@outlook.com",
+                    Email = "radp2016@outlook.com",
+                    EmailConfirmed = true,
+                    JoinDate = DateTime.Now,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    FirstName = "Rad",
+                    Surname = "Paulner",
+                    PasswordHash = ps.HashPassword("radP2016$1")
+                });
+            context.SaveChanges();
+
+            ApplicationUser admin = manager.FindByEmail("powell.paul@itsligo.ie");
+            if (admin != null)
+            {
+                manager.AddToRoles(admin.Id, new string[] { "Admin", "member", "ClubAdmin" });
+            }
+        }
+    }
+}
